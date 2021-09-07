5b5aa1f4e5529fd619382977d6e94efdb4bb743b
Daejv <72608569+Daejv@users.noreply.github.com>
2021-09-07 13:54:17 +0200

    Add files via upload

---
+++ 07.09.2021/wedkuj.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Wędkujemy</title>
+    <link rel="stylesheet" href="styl_1.css">
+</head>
+<body>
+    <header>
+        <h1>Portal dla wędkarzy</h1>
+    <div id="lewy">
+        <h2>Ryby drapieżne naszych wód</h2>
+       
+        
+        <ul>
+        
+    <?php
+        $lacze=mysqli_connect("localhost" , "root", "", "wedkowanie");
+        $pytanie="SELECT nazwa , wystepowanie FROM ryby WHERE styl_zycia = 1";
+        $wynik=mysqli_query ($lacze, $pytanie);
+        while($wiersz = mysqli_fetch_row($wynik)) {
+            echo "<li>" .$wiersz[0]. ", wystepowanie: " .$wiersz[1]."</li>" ; 
+        }
+        mysqli_close($lacze);
+    ?>
+        </ul>
+
+    </div>
+    <div id="prawy">    
+        <img src="ryba1.jpg" alt="sum">
+        <br>
+    <p style="text-align: justify">
+        <a href="kwerendy.txt">Pobierz kwerendy</a>
+    </p> 
+    </div>
+    <footer>
+            <p>Strone wykonal: 00000000000</p>
+    </footer>
+
+     
+</body>
+</html>
